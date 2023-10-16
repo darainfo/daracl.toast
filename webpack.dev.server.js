614575939119d35490d23efb0f5731ccb6408f25
darainfo <darainfosys@gmail.com>
2023-10-16 09:58:03 +0900

    reload 옵션

--- webpack.dev.server.js
+++ webpack.dev.server.js
@@ -1,40 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const merge = require('webpack-merge').merge;
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge").merge;
+const common = require("./webpack.common.js");
 
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist')
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
 };
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   output: {
-    path: path.join(__dirname, './public'),
-    filename: 'dara.toast.js',
-    libraryTarget: 'umd',
-  }
-  , devServer: {
+    path: path.join(__dirname, "./public"),
+    filename: "dara.toast.js",
+    libraryTarget: "umd",
+  },
+  devServer: {
     // inline: true,
     //mode: 'development',
-    host: '0.0.0.0',
-    port: 8888,
+    host: "0.0.0.0",
+    port: 8887,
+    watchFiles: ["src/**/*"],
   },
   plugins: [
     //new BundleAnalyzerPlugin()
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'dist', to: 'dist' }
-      ]
-    })
-
-  ]
+      patterns: [{ from: "dist", to: "dist" }],
+    }),
+  ],
 });
